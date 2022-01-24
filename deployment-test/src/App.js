b5cc99a3992bea06c9c3d196c733a2c0c657c9ec
QuackZ90 <82066443+QuackZ90@users.noreply.github.com>
2022-01-24 12:25:01 +0800

    included env

--- deployment-test/src/App.js
+++ deployment-test/src/App.js
@@ -1,6 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const env = process.env.REACT_APP_ENV;
+
+let config = {
+  baseURL:"http://localhost/",
+}
+
+
+switch (env.toUpperCase()){
+  case "STAGE":{
+    config.baseURL = "https://stage.QuackZ90.github.io/deployment-test";
+    break;
+  }
+
+  case "PRODUCTION":{
+    config.baseURL = "https://QuackZ90.github.io/deployment-test";
+    break;
+  }
+}
+
 function App() {
   return (
     <div className="App">
